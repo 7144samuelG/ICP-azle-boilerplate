1ab07d83bb89dbe0d899e4f8e9d874d8671dcb07
Eric <eric.vincentius.jaolis@gmail.com>
2024-12-25 10:09:47 +0700

    First commit

--- src/hello_backend/main.mo
+++ src/hello_backend/main.mo
@@ -1,5 +1,35 @@
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Text "mo:base/Text";
+
 actor {
-  public query func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+  stable var anne_voters: Nat = 0;
+  Debug.print(debug_show(anne_voters));
+
+  stable var jenni_voters: Nat = 0;
+  Debug.print(debug_show(jenni_voters));
+
+  stable var mike_voters: Nat = 0;
+  Debug.print(debug_show(mike_voters));
+
+  stable var thomas_voters: Nat = 0;
+  Debug.print(debug_show(thomas_voters));
+
+  var start_time = Time.now();
+  Debug.print(debug_show(start_time));
+
+  public func add_voters(candidate: Text) {
+    if(Text.equal(Text.toLowercase(candidate), Text.toLowercase("Anne"))) {anne_voters += 1};
+    if(Text.equal(Text.toLowercase(candidate), Text.toLowercase("Jenni"))) {jenni_voters += 1};
+    if(Text.equal(Text.toLowercase(candidate), Text.toLowercase("Mike"))) {mike_voters += 1};
+    if(Text.equal(Text.toLowercase(candidate), Text.toLowercase("Thomas"))) {thomas_voters += 1};
+  };
+
+  public query func checkVoters(candidate: Text): async Nat {
+    if(Text.equal(Text.toLowercase(candidate), Text.toLowercase("Anne"))) return anne_voters;
+    if(Text.equal(Text.toLowercase(candidate), Text.toLowercase("Jenni"))) return jenni_voters;
+    if(Text.equal(Text.toLowercase(candidate), Text.toLowercase("Mike"))) return mike_voters;
+    if(Text.equal(Text.toLowercase(candidate), Text.toLowercase("Thomas"))) return thomas_voters;
+    return 0;
   };
 };
--- src/hello_frontend/src/main.js
+++ src/hello_frontend/src/main.js
@@ -1,4 +1,42 @@
 import App from './App';
 import './index.scss';
+import { UpdateCallRejectedError } from "@dfinity/agent";
+import { hello_backend } from "../../declarations/hello_backend";
 
 const app = new App();
+
+document.querySelector(".vote-anne").addEventListener("click", async function(event) {
+    await hello_backend.add_voters("Anne");
+    displayVoters();
+})
+
+document.querySelector(".vote-jenni").addEventListener("click", async function(event) {
+    await hello_backend.add_voters("Jenni");
+    displayVoters();
+})
+
+document.querySelector(".vote-mike").addEventListener("click", async function(event) {
+    await hello_backend.add_voters("Mike");
+    displayVoters();
+})
+
+document.querySelector(".vote-thomas").addEventListener("click", async function(event) {
+    await hello_backend.add_voters("Thomas");
+    displayVoters();
+})
+
+async function displayVoters() {
+    const votersNumber = document.querySelectorAll(".voters-number");
+    votersNumber.forEach(part => {
+        part.style.display = "inline";
+    });
+
+    const anneVoters = await hello_backend.checkVoters("anne");
+    const jenniVoters = await hello_backend.checkVoters("jenni");
+    const mikeVoters = await hello_backend.checkVoters("mike");
+    const thomasVoters = await hello_backend.checkVoters("thomas");
+    document.querySelector(".anne-voters").innerText = anneVoters;
+    document.querySelector(".jenni-voters").innerText = jenniVoters;
+    document.querySelector(".mike-voters").innerText = mikeVoters;
+    document.querySelector(".thomas-voters").innerText = thomasVoters;
+}
